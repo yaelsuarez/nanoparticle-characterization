836318bda489fea3825f65babb82bdb029be5667
Roy Rinberg <royrinberg@gmail.com>
2021-10-14 11:01:08 -0400

    fix the setup.py, and add a potenitally unecessaryinit.py

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='kira',
-    version='0.0',
-    description='Analysis of characterization results',
-    author='Yael Suarez',
-    author_email='yaeldelcarmen.suarezlopez@ki.se',
-    url=' ',
-    packages=find_packages(where='src'),
-    entry_points={  
-    'console_scripts': [
-            'xrd=kira.xrd.plot:plot_folder',
-            'nir=kira.nir.plot:plot_folder',
-            'mean_fl=kira.mean_fl.plot:plot_folder',
-            'bet=kira.bet.radius:cli_bet_radius',
-        ],
-    },
-    install_requires=[
-        "numpy",
-        "matplotlib",
-        "pyyaml"
-    ]
-)
-
+setup(name='kira',
+      version='0.1',
+      description='Analysis of characterization results',
+      author='Yael Suarez',
+      author_email='yaeldelcarmen.suarezlopez@ki.se',
+      url=' ',
+      packages=["bet", "model"],
+      package_dir={
+          "": ".",
+          "bet": "./kira/bet",
+          "model": "./kira/model",
+      },
+      entry_points={
+          'console_scripts': [
+              'bet=bet.radius:cli_bet_radius',
+          ],
+      },
+      install_requires=["numpy", "matplotlib", "pyyaml"])
